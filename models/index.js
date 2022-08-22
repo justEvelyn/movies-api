31f63fa60bd9f808cb377ab746efd519abbe6e1f
Evelyn Boansi <evelynboansi@hotmail.com>
2022-08-22 20:58:25 +0100

    testing

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const {User} = require('./User')
 User.belongsToMany(Movie, {through: "reviews"})
 Movie.belongsToMany(User, {through: "reviews"})
 
-
+//swag testingg 
 
 
 module.exports = { Movie, User}
